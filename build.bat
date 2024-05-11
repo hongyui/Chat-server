68ae24bbc7ed334363d69d6007efe7ec0ca3c5f1
HongYui <jerrytw36@gmail.com>
2024-05-11 19:49:02 +0800

    Summary

--- build.bat
+++ build.bat
@@ -1,14 +1,15 @@
 @echo off
 echo configuring project...
-cmake -B build -S . > cmake_output.txt 2>&1 || (
+PowerShell -Command "cmake -B build -S . 2>&1 | Tee-Object -FilePath cmake_output.txt" || (
     findstr /C:"vcpkg/scripts/buildsystems/vcpkg.cmake" cmake_output.txt >nul || (
         echo cmake failed, see cmake_output.txt for details
         pause
         exit /b
     )
     echo Detected vcpkg error, running vcpkg_install.bat
-    call "script/vcpkg_install.bat"
+    PowerShell -Command "script/vcpkg_install.bat 2>&1 | Tee-Object -FilePath CON"
 )
+
 echo building project...
 cmake --build build
 pause
